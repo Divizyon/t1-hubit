da03d297b7f28cde8647ce81bcf5f9de015774b4
melike <melikekaymak27@gmail.com>
2025-05-06 21:25:45 +0300

    sosyal inovasyonun konumu degişti +develop

--- src/javascript/World/index.js
+++ src/javascript/World/index.js
@@ -938,7 +938,7 @@ export default class World {
     this.brick = this.objects.add({
       base: this.resources.items.sosyalinovasyonBase.scene,
       collision: this.resources.items.brickCollision.scene,
-      offset: new THREE.Vector3(25, 25, 0),
+      offset: new THREE.Vector3(82, 50, 0),
       rotation: new THREE.Euler(Math.PI/2, Math.PI, 0),
       shadow: { sizeX: 1.5, sizeY: 1.5, offsetZ: -0.6, alpha: 0.4 },
       mass: 1.5,
