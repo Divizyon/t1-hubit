386cae53884305aa4f10e93e9104147aabeff493
Süreyya Bayrak <sureyyart.design@gmail.com>
2025-05-11 19:44:59 +0300

    The color of AtmosferAlani was set

--- src/javascript/World/Objects.js
+++ src/javascript/World/Objects.js
@@ -2,10 +2,8 @@ import * as THREE from 'three'
 import * as BufferGeometryUtils from 'three/addons/utils/BufferGeometryUtils.js';
 
 
-export default class Objects
-{
-    constructor(_options)
-    {
+export default class Objects {
+    constructor(_options) {
         // Options
         this.time = _options.time
         this.resources = _options.resources
@@ -26,24 +24,21 @@ export default class Objects
         this.setMerge()
     }
 
-    setParsers()
-    {
+    setParsers() {
         this.parsers = {}
 
         this.parsers.items = [
             // Shade
             {
                 regex: /^shade([a-z]+)_?[0-9]{0,3}?/i,
-                apply: (_mesh, _options) =>
-                {
+                apply: (_mesh, _options) => {
                     // Find material
                     const match = _mesh.name.match(/^shade([a-z]+)_?[0-9]{0,3}?/i)
                     const materialName = `${match[1].substring(0, 1).toLowerCase()}${match[1].substring(1)}` // PastalCase to camelCase
                     let material = this.materials.shades.items[materialName]
 
                     // Default
-                    if(typeof material === 'undefined')
-                    {
+                    if (typeof material === 'undefined') {
                         material = new THREE.MeshNormalMaterial()
                     }
 
@@ -51,12 +46,9 @@ export default class Objects
                     const mesh = _options.duplicated ? _mesh.clone() : _mesh
                     mesh.material = material
 
-                    if(mesh.children.length)
-                    {
-                        for(const _child of mesh.children)
-                        {
-                            if(_child instanceof THREE.Mesh)
-                            {
+                    if (mesh.children.length) {
+                        for (const _child of mesh.children) {
+                            if (_child instanceof THREE.Mesh) {
                                 _child.material = material
                             }
                         }
@@ -67,11 +59,10 @@ export default class Objects
             },
             {
                 regex: /^hubit([a-z]+)_?[0-9]{0,3}?/i,
-                apply: (_mesh, _options) =>
-                {
+                apply: (_mesh, _options) => {
                     // Find material
                     const match = _mesh.name.match(/^hubit([a-z]+)_?[0-9]{0,3}?/i)
-                    
+
                     // Materyal adlarını düzelt
                     // let materialName = ""
                     //hubit olduğunda burası kullanılacak
@@ -85,18 +76,17 @@ export default class Objects
                     //     const colorPart = match[1]
                     //     materialName = "hubit" + colorPart
                     // }
-                    
+
                     // Önce this.materials.shades.items içinde ara
                     // let material = this.materials.shades.items[materialName]
-                    
+
                     // Bulunamazsa this.materials.items içinde ara
                     // if(typeof material === 'undefined') {
                     //     material = this.materials.items[materialName]
                     // }
 
                     // Default
-                    if(typeof material === 'undefined')
-                    {
+                    if (typeof material === 'undefined') {
                         material = new THREE.MeshNormalMaterial()
                     }
 
@@ -111,16 +101,14 @@ export default class Objects
             // Shade
             {
                 regex: /^pure([a-z]+)_?[0-9]{0,3}?/i,
-                apply: (_mesh, _options) =>
-                {
+                apply: (_mesh, _options) => {
                     // Find material
                     const match = _mesh.name.match(/^pure([a-z]+)_?[0-9]{0,3}?/i)
                     const materialName = match[1].toLowerCase()
                     let material = this.materials.pures.items[materialName]
 
                     // Default
-                    if(typeof material === 'undefined')
-                    {
+                    if (typeof material === 'undefined') {
                         material = new THREE.MeshNormalMaterial()
                     }
 
@@ -135,8 +123,7 @@ export default class Objects
             // Floor
             {
                 regex: /^floor_?[0-9]{0,3}?/i,
-                apply: (_mesh, _options) =>
-                {
+                apply: (_mesh, _options) => {
                     // Create floor manually because of missing UV
                     const geometry = new THREE.PlaneGeometry(_mesh.scale.x, _mesh.scale.y, 10, 10)
                     const material = this.materials.items.floorShadow.clone()
@@ -158,8 +145,7 @@ export default class Objects
 
         // Default
         this.parsers.default = {}
-        this.parsers.default.apply = (_mesh) =>
-        {
+        this.parsers.default.apply = (_mesh) => {
             // Create clone mesh with normal material
             const mesh = _mesh.clone()
             mesh.material = this.materials.shades.items.white
@@ -168,8 +154,7 @@ export default class Objects
         }
     }
 
-    setMerge()
-    {
+    setMerge() {
         this.merge = {}
         this.merge.items = {}
 
@@ -177,13 +162,11 @@ export default class Objects
         this.merge.container.matrixAutoUpdate = false
         this.container.add(this.merge.container)
 
-        this.merge.add = (_name, _mesh) =>
-        {
+        this.merge.add = (_name, _mesh) => {
             let mergeItem = this.merge.items[_name]
 
             // Create merge item if not found
-            if(!mergeItem)
-            {
+            if (!mergeItem) {
                 mergeItem = {}
 
                 // Geometry
@@ -210,10 +193,8 @@ export default class Objects
             mergeItem.geometriesToMerge.push(geometry)
         }
 
-        this.merge.applyMerge = () =>
-        {
-            for(const _mergeItemName in this.merge.items)
-            {
+        this.merge.applyMerge = () => {
+            for (const _mergeItemName in this.merge.items) {
                 const mergeItem = this.merge.items[_mergeItemName]
 
                 mergeItem.geometry = BufferGeometryUtils.mergeGeometries(mergeItem.geometriesToMerge) // Should add original geometry
@@ -221,18 +202,13 @@ export default class Objects
             }
         }
 
-        this.merge.update = () =>
-        {
-            for(const _object of this.items)
-            {
-                if(_object.shouldMerge)
-                {
+        this.merge.update = () => {
+            for (const _object of this.items) {
+                if (_object.shouldMerge) {
                     const children = [..._object.container.children]
-                    for(const _child of children)
-                    {
+                    for (const _child of children) {
                         const materialName = _child.material.name
-                        if(materialName !== '')
-                        {
+                        if (materialName !== '') {
                             this.merge.add(materialName, _child)
 
                             // Remove from parent
@@ -251,33 +227,42 @@ export default class Objects
         }
     }
 
-    getConvertedMesh(_children, _options = {})
-    {
+    getConvertedMesh(_children, _options = {}) {
         const container = new THREE.Object3D()
         const center = new THREE.Vector3()
 
         // Go through each base child
         const baseChildren = [..._children]
 
-        for(const _child of baseChildren)
-        {
+        for (const _child of baseChildren) {
             // Find center
-            if(_child.name.match(/^center_?[0-9]{0,3}?/i))
-            {
+            if (_child.name.match(/^center_?[0-9]{0,3}?/i)) {
                 center.set(_child.position.x, _child.position.y, _child.position.z)
             }
 
-            if(_child instanceof THREE.Mesh)
-            {
-                // Find parser and use default if not found
-                let parser = this.parsers.items.find((_item) => _child.name.match(_item.regex))
-                if(typeof parser === 'undefined')
-                {
-                    parser = this.parsers.default
-                }
+            if (_child instanceof THREE.Mesh) {
+                let mesh;
 
-                // Create mesh by applying parser
-                const mesh = parser.apply(_child, _options)
+                // Eğer materyal korunacaksa parser kullanma
+                if (_options.preserveMaterials) {
+                    mesh = _child.clone();
+                    if (_child.material) {
+                        if (Array.isArray(_child.material)) {
+                            mesh.material = _child.material.map(mat => mat.clone());
+                        } else {
+                            mesh.material = _child.material.clone();
+                        }
+                    }
+                } else {
+                    // Find parser and use default if not found
+                    let parser = this.parsers.items.find((_item) => _child.name.match(_item.regex))
+                    if (typeof parser === 'undefined') {
+                        parser = this.parsers.default
+                    }
+
+                    // Create mesh by applying parser
+                    mesh = parser.apply(_child, _options)
+                }
 
                 // Add to container
                 container.add(mesh)
@@ -285,18 +270,15 @@ export default class Objects
         }
 
         // Recenter
-        if(center.length() > 0)
-        {
-            for(const _child of container.children)
-            {
+        if (center.length() > 0) {
+            for (const _child of container.children) {
                 _child.position.sub(center)
             }
 
             container.position.add(center)
         }
 
-        if(_options.mass && _options.mass === 0)
-        {
+        if (_options.mass && _options.mass === 0) {
             container.matrixAutoUpdate = false
             container.updateMatrix()
         }
@@ -304,8 +286,7 @@ export default class Objects
         return container
     }
 
-    add(_options)
-    {
+    add(_options) {
         const object = {}
 
         object.merged = false
@@ -313,15 +294,13 @@ export default class Objects
 
         // Offset
         const offset = new THREE.Vector3()
-        if(_options.offset)
-        {
+        if (_options.offset) {
             offset.copy(_options.offset)
         }
 
         // Rotation
         const rotation = new THREE.Euler()
-        if(_options.rotation)
-        {
+        if (_options.rotation) {
             rotation.copy(_options.rotation)
         }
 
@@ -341,13 +320,11 @@ export default class Objects
         this.container.add(object.container)
 
         // Deactivate matrix auto update
-        if(_options.mass === 0)
-        {
+        if (_options.mass === 0) {
             object.container.matrixAutoUpdate = false
             object.container.updateMatrix()
 
-            for(const _child of object.container.children)
-            {
+            for (const _child of object.container.children) {
                 _child.matrixAutoUpdate = false
                 _child.updateMatrix()
             }
@@ -355,18 +332,17 @@ export default class Objects
 
         // Create physics object
         if (_options.collision && _options.collision.children && _options.collision.children.length > 0) {
-        object.collision = this.physics.addObjectFromThree({
-            meshes: [..._options.collision.children],
-            offset,
-            rotation,
-            mass: _options.mass,
-            sleep
-        })
+            object.collision = this.physics.addObjectFromThree({
+                meshes: [..._options.collision.children],
+                offset,
+                rotation,
+                mass: _options.mass,
+                sleep
+            })
 
-        for(const _child of object.container.children)
-        {
+            for (const _child of object.container.children) {
                 if (object.collision && object.collision.center) {
-            _child.position.sub(object.collision.center)
+                    _child.position.sub(object.collision.center)
                 }
             }
         } else {
@@ -374,7 +350,7 @@ export default class Objects
             if (_options.name) {
                 console.info(`${_options.name} modeli için fizik nesnesi oluşturulmadı, içinden geçilebilir olacak`);
             }
-            
+
             // Fizik nesnesi olmadan bir merkez tanımla
             object.collision = {
                 center: new THREE.Vector3(0, 0, 0),
@@ -383,10 +359,8 @@ export default class Objects
         }
 
         // Sound
-        if(_options.soundName && object.collision && object.collision.body)
-        {
-            object.collision.body.addEventListener('collide', (_event) =>
-            {
+        if (_options.soundName && object.collision && object.collision.body) {
+            object.collision.body.addEventListener('collide', (_event) => {
                 const relativeVelocity = _event.contact.getImpactVelocityAlongNormal()
                 this.sounds.play(_options.soundName, relativeVelocity)
             })
@@ -394,16 +368,13 @@ export default class Objects
 
         // Shadow
         // Add shadow
-        if(_options.shadow)
-        {
+        if (_options.shadow) {
             this.shadows.add(object.container, _options.shadow)
         }
 
         // Time tick event
-        if(_options.mass > 0 && object.collision && object.collision.body)
-        {
-            this.time.on('tick', () =>
-            {
+        if (_options.mass > 0 && object.collision && object.collision.body) {
+            this.time.on('tick', () => {
                 object.container.position.copy(object.collision.body.position)
                 object.container.quaternion.copy(object.collision.body.quaternion)
             })
